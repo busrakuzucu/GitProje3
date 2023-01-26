7b6c0b6ec45201144d951f195551e3ac32f61fe7
busrakuzucu <busrakuzucu94@gmail.com>
2023-01-26 19:24:32 +0300

    First task has done!

---
+++ src/Gun1/Okul.java
@@ -0,0 +1,4 @@
+package Gun1;
+
+public class Okul {
+}
---
+++ src/Gun1/Student.java
@@ -0,0 +1,7 @@
+package Gun1;
+
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("Merhaba1 ");
+    }
+}
