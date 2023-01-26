4b40ddaffa7405ed1a65b973f8e5a44505b677cd
busrakuzucu <busrakuzucu94@gmail.com>
2023-01-26 19:29:55 +0300

    Second task has done

--- src/Gun1/Student.java
+++ src/Gun1/Student.java
@@ -3,5 +3,7 @@ package Gun1;
 public class Student {
     public static void main(String[] args) {
         System.out.println("Merhaba1 ");
+        System.out.println("Merhaba2");
+        System.out.println("second task has done");
     }
 }
