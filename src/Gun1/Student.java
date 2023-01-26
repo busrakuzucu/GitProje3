af31222f164d83534a4d16c0f7dc5f277724784d
busrakuzucu <busrakuzucu94@gmail.com>
2023-01-26 19:39:11 +0300

    studentta bir satır silindi güncel

--- src/Gun1/Student.java
+++ src/Gun1/Student.java
@@ -3,7 +3,7 @@ package Gun1;
 public class Student {
     public static void main(String[] args) {
         System.out.println("Merhaba1 ");
-        System.out.println("Merhaba2");
+        //merhaba 2 silindi
         System.out.println("second task has done");
     }
 }
